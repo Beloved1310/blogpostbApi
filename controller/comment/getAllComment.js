a19a687f77fbed6e4c02ec5d191fb63c0e8b567d
Beloved1310 <adeoluwafisayomi@gmail.com>
2021-09-19 02:38:27 +0100

    docs: change get all comment controller

--- controller/comment/getAllComment.js
+++ controller/comment/getAllComment.js
@@ -1,7 +1,7 @@
 const Blog = require('../../Model/Blog')
 
 module.exports = async (req, res) => {
-  const result = await Blog.findById(req.params.id).select('comments')
+  const result = await Blog.findById(req.params.id).populate('comments')
   const data = { data: result.comments }
   return res.status(200).json(data)
 }
