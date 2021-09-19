a572fe8a01eb327b9c0cf9d13ac5ac6ee819d3e7
Beloved1310 <adeoluwafisayomi@gmail.com>
2021-09-19 02:39:15 +0100

    docs: change get one comment controller

--- controller/comment/getOneComment.js
+++ controller/comment/getOneComment.js
@@ -1,24 +1,19 @@
 /* eslint no-underscore-dangle: "off" */
+/* eslint eqeqeq: "off" */
 
 const Blog = require('../../Model/Blog')
 
-// module.exports = async (req, res) => {
-//     console.log(req.params.id)
-//   const result = await Blog.find({'comments': req.params._id})
+module.exports = async (req, res) => {
+  const { postId, commentId } = req.params
 
-//   return res.status(200).json(result);
+  const post = await Blog.findById(postId).populate('comments')
+  if (!post) res.send({ message: 'Invalid id' })
 
-// }
-// // Blog.findOne({comments: { $all: [req.params.id] }})
+  const data = post.comments.find((a) => a._id == commentId)
 
-module.exports = (req, res) => {
-  const { postId } = req.params
-  const { commentId } = req.params
+  const getOnedata = {
+    data,
+  }
 
-  Blog.findById(postId, (error, post) => {
-    if (error) res.send(error)
-    const data = post.comments.find((a) => a._id === commentId)
-
-    return res.status(200).send({ message: data })
-  })
+  return res.status(200).send(getOnedata)
 }
